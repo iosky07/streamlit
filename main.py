2a8cf2bb0a929306910cb0e848eeb55ae99548f5
iosky07 <yoskitanjung07@gmail.com>
2023-03-16 17:39:19 +0700

    add result card
    css integration

--- main.py
+++ main.py
@@ -14,7 +14,68 @@ st.set_page_config(
     page_icon="OOO"
 )
 
-st.title("Pencarian Artikel")
+with open('custom.css') as source_des:
+    st.markdown("<style>{source_des.read()}</style>", unsafe_allow_html=True)
+
+CSS_LINKS = """<link href="assets/img/favicon.png" rel="icon">
+<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="assets/css/main.css" rel="stylesheet">
+"""
+
+CSS_TABLE_LISTS = """
+<div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="pricing-item">
+              <h3>Free Plan</h3>
+              <h4><sup>$</sup>0<span> / month</span></h4>
+              <ul>
+                <li><i class="bi bi-check"></i> Quam adipiscing vitae proin</li>
+                <li><i class="bi bi-check"></i> Nec feugiat nisl pretium</li>
+                <li><i class="bi bi-check"></i> Nulla at volutpat diam uteera</li>
+                <li class="na"><i class="bi bi-x"></i> <span>Pharetra massa massa ultricies</span></li>
+                <li class="na"><i class="bi bi-x"></i> <span>Massa ultricies mi quis hendrerit</span></li>
+              </ul>
+              <a href="#" class="buy-btn">Buy Now</a>
+            </div>
+          </div>"""
+
+SEARCH_RESULT = """
+<div>
+<h4>{}</h4>
+<h4>{}</h4>
+<h4>{}</h4>
+<h4>{}</h4>
+<h4>{}</h4>
+</div>
+"""
+
+# st.markdown(CSS_LINKS, unsafe_allow_html=True)
+
+st.markdown("""
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+""", unsafe_allow_html=True)
+
+def card(similarity, title, p_date):
+    return f"""
+        <div class="card">
+          <h5 style="color:black;" class="card-header">Kemiripan: {similarity}%</h5>
+          <div class="card-body">
+            <h5 style="color:black;" class="card-title">{title}</h5>
+            <p style="color:black;" class="card-text">{p_date}</p>
+            <a style="color:white;" href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <br>
+    """
+
 st.sidebar.success("Select a page above.")
 
 df = pd.read_csv('JELIKU-1.csv')
@@ -101,6 +162,7 @@ if submit:
     for sum in range(79):
         temp = sum_d_akar_kuadrat[sum] * sum_d_akar_kuadrat[3]
         temp1 = sum_d[sum] / temp
+        temp1 = round((temp1 * 100), 2)
 
         cossim_result.append(temp1)
 
@@ -116,5 +178,15 @@ if submit:
 
     st.write("Hasil perhitungan Cosine Similarity: ")
 
-    st.write(res)
+    # st.write(res)
+    for r in res:
+        # st.markdown(CSS_TABLE_LISTS, unsafe_allow_html=True)
+        title = r[1]['Judul']
+        abstract = r[1]['Abstrak']
+        p_date = r[1]['Tanggal']
+        author = r[1]['Author']
+        journal = r[1]['Jurnal']
+        similarity = r[1]['similarity']
+        st.markdown(card(similarity, title, p_date), unsafe_allow_html=True)
+
 
