494915be0dfea39376b30bcad4a046bdb2d195ea
Yoski Tanjung <yoskitanjung07@gmail.com>
2023-07-14 18:03:25 +0700

    delete plotly express import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import io
 import itertools
 
 import streamlit as st
-import plotly.express as px
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
